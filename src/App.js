637ee417cf26926f3b70d43f90434bcbc6c98a02
gortatka <gtatsiana@tut.by>
2021-02-23 00:31:19 +0300

    add contact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from './components/Contact';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact avatar="https://zvetnoe.ru/upload/images/blog/Almazka/cat.jpg" 
+        name="Cat"
+        online={true} />
+       
+     
+      
     </div>
   );
 }
 
-export default App;
